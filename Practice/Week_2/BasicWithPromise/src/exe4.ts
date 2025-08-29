1d0fce6527efa19769ba0165375903575322290f
Thien Do <dnnthien2903@gmail.com>
2025-08-29 20:05:07 +0700

    Use .then() and .catch() to handle a Promise that returns a random number

---
+++ Practice/Week_2/BasicWithPromise/src/exe4.ts
@@ -0,0 +1,18 @@
+function randomNumber(): Promise<number> {
+    return new Promise((resolve, reject) => {
+        var num = Math.floor(Math.random() * 10);
+        if(num >= 5) {
+            console.log('RESOLVE');
+            resolve(num);
+        } else {
+            console.log('REJECT');
+            reject(num);
+        }
+    })
+}
+
+randomNumber().then(num => {
+    console.log(num);
+}).catch(num => {
+    console.log(num);
+})
